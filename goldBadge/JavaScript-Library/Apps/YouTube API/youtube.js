1b7289821aa756a8f804bb5267a4f4422ec3dad0
aspenduvall <40346050+aspenduvall@users.noreply.github.com>
2021-10-26 18:19:06 -0400

    Delete goldBadge directory

--- goldBadge/JavaScript-Library/Apps/YouTube API/youtube.js
+++
@@ -1,3 +0,0 @@
-const searchTerm = document.querySelector('.search');
-const searchForm = document.querySelector('form');
-const submitBtn = document.querySelector('.submit)');
--- goldBadge/JavaScript-Library/JavaScript-Basics/08-arrays.js
+++
@@ -1,4 +0,0 @@
-var secretPlan = ["run away forever", "buy a new car", "move to Fishers"];
-
-console.log(secretPlan[1]);
-console.log("Secret Plan: ", secretPlan[2]);
