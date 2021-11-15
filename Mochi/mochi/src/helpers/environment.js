4d7b16369820089cf767e22236a4e92f49261fba
Aspen Duvall <aspenduvall@gmail.com>
2021-11-15 16:58:44 -0500

    updated environment.js

--- Mochi/mochi/src/helpers/environment.js
+++ Mochi/mochi/src/helpers/environment.js
@@ -1,12 +1,12 @@
 let APIURL = '';
-switch (window.location.localhost) {
+switch (window.location.hostname) {
     // this is the local host name of your react app
     case 'localhost' || '127.0.0.1':
         // this is the local host name of your API
-        APIURL = 'http://localhost:3000';
+        APIURL = 'http://localhost:5432';
         break;
     // this is the deployed react application
-    case 'amd-mochi.herokuapp.com':
+    case 'amd-mochi-client.herokuapp.com':
         // this is the full url of your deployed API
         APIURL = 'https://amd-mochi.herokuapp.com'
 }
