30071b161156e0c553c5a1fbcc91b6d280403f2c
Aspen Duvall <aspenduvall@gmail.com>
2021-11-15 17:06:28 -0500

    deploying mochi 1.0

--- Mochi/mochi/src/helpers/environment.js
+++ Mochi/mochi/src/helpers/environment.js
@@ -3,7 +3,7 @@ switch (window.location.hostname) {
     // this is the local host name of your react app
     case 'localhost' || '127.0.0.1':
         // this is the local host name of your API
-        APIURL = 'http://localhost:5432';
+        APIURL = 'http://localhost:3000';
         break;
     // this is the deployed react application
     case 'amd-mochi-client.herokuapp.com':
